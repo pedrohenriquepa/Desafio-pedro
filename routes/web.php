9b35b92beda4a45b4e455da5e4be2b2a563b35c6
Bruno <brnofreire@gmail.com>
2025-04-24 11:40:32 -0300

    estrutura inicial do projeto

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('web', 'auth')->group(function () {
+    Route::get('/desafio-avelar', [Controller::class, 'index'])->name('desafio.avelar.index');
 });
